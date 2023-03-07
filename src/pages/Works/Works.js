37e59e5a54ced3ac295f44669d6c7cbbf9f270f6
David Isidore <isidoredavid@protonmail.com>
2023-03-07 10:27:31 +0100

    Implémentation des liens de la page Réalisations

--- src/pages/Works/Works.js
+++ src/pages/Works/Works.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import { Link } from 'react-router-dom'
 
 export default function Works() {
     return (
         <div>
             <Header />
+            <div>
+                <h1>Mes projets développés</h1>
+                <ul>
+                    <li>
+                        <Link to="https://davidisidore.github.io/Livrables-P2/">
+                            Booki
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="https://davidisidore.github.io/Projet3/">
+                            OhMyFood
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="https://davidisidore.github.io/Kasa/">
+                            Kasa
+                        </Link>
+                    </li>
+                </ul>
+            </div>
             <Footer />
         </div>
     )
