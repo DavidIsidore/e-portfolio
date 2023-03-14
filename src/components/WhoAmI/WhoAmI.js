7e20559ca6fd5efdb8c75590c19abe7f86f74314
David Isidore <isidoredavid@protonmail.com>
2023-03-14 10:40:26 +0100

    Implémentation de la page WhoAmI

---
+++ src/components/WhoAmI/WhoAmI.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function WhoAmI() {
+    return (
+        <div>
+            <h1>Qui suis-je</h1>
+            <p></p>
+        </div>
+    )
+}
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import WhoAmI from '../../components/WhoAmI/WhoAmI'
 
 export default function About() {
     return (
         <div>
             <Header />
+            <WhoAmI />
             <Footer />
         </div>
     )
