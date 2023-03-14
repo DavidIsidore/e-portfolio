ac0b60c273b201aeab96a57282727d438285fc09
David Isidore <isidoredavid@protonmail.com>
2023-03-14 10:44:14 +0100

    Ajout du texte du composant WhoAmI

--- src/components/WhoAmI/WhoAmI.js
+++ src/components/WhoAmI/WhoAmI.js
@@ -4,7 +4,72 @@ export default function WhoAmI() {
     return (
         <div>
             <h1>Qui suis-je</h1>
-            <p></p>
+            <p>
+                Après un BTS Traitements de Surfaces obtenu en 1997, je passe
+                quasiment 20 ans dans différentes branches de l’industrie :
+                tôlerie fine, peinture, nickel électrolytique et chimique pour
+                finir par de l’usinage chimique dans le secteur aéronautique.
+            </p>
+            <p>
+                C’est le moment où mon aîné commence à aller à l’école mais,
+                avec mon épouse, nous nous rendons compte pendant sa deuxième
+                année de maternelle que ce système ne lui convient pas. Nous
+                décidons de déménager de Colomiers vers Albi (je garde mon
+                emploi sur la région toulousaine, travaillant de week-end à
+                cette période de ma vie) pour le placer en école Montessori . Je
+                commence à m’intéresser à cette pédagogie pour comprendre
+                comment il y travaille et je me forme auprès de l’association
+                « Le chemin de l’enfant ».
+            </p>
+            <p>
+                L’éloignement avec la famille et le décalage d’emplois du temps
+                ont raison de ma volonté de continuer dans mon secteur
+                d’activité et je crée mon auto-entreprise qui propose du soutien
+                scolaire niveau collège et des ateliers Montessori à destination
+                d’enfants trisomiques. La directrice de l’école dans laquelle
+                sont scolarisés mes enfants me prête les locaux de l’école (je
+                ne la remercierai jamais assez) pour mon activité qui se déroule
+                le mercredi. Le reste de la semaine, je l’occupe en donnant un
+                coup de main à l’école (surveillance pendant les temps de repas
+                et de garderie). Lorsqu’une collègue tombe malade, la directrice
+                me propose de m’embaucher au sein de l’école et c’est le départ
+                d’une nouvelle aventure qui durera 4 années.
+            </p>
+            <p>
+                Malheureusement, le confinement pendant la pandémie et des
+                problèmes de santé me forceront à arrêter cette activité. Nous
+                faisons le choix avec mon épouse de nous « mettre au vert » et
+                de ralentir le rythme effréné dans lequel nous nous étions
+                enfermés et nous déménageons une fois de plus, dans le Cantal.
+            </p>
+            <p>
+                Je décide pour me réorienter de revenir à mes premières amours :
+                l’informatique.
+            </p>
+            <p>
+                Je pense m’orienter vers le community management pour, entre
+                autres, seconder mon épouse qui a entretemps créé son
+                auto-entreprise également. Mais je me rends compte, en faisant
+                quelques recherches avant de choisir la plateforme de formation,
+                que la gestion de contenu sur les réseaux sociaux ne m’est pas
+                aussi aisée que je l’avais pensé au départ. L’évidence s’impose
+                clairement à moi : puisque j’ai aidé mon épouse et une amie à
+                mettre leur site en place sous Wordpress, je n’ai qu’à continuer
+                dans cette voie. Mon choix est alors fait : je me tourne vers le
+                développement Web.
+            </p>
+            <p>
+                En quelques jours, le dossier est monté auprès de Pôle Emploi et
+                je débute officiellement ma formation chez OpenClassrooms le 1er
+                juillet 2022. Finalement, après quelques frayeurs pour avoir
+                pris un peu de retard sur un projet, je vais pouvoir terminer ma
+                formation avec une semaine d’avance sur le programme prévu, le
+                21 février.
+            </p>
+            <p>
+                Il est temps maintenant de rejoindre le grand bain de la
+                recherche d’emploi.
+            </p>
         </div>
     )
 }
