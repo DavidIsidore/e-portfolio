ee6441d8236287c0a02602b98cb0808a8272a442
David Isidore <isidoredavid@protonmail.com>
2023-03-14 11:51:35 +0100

    Modification de la mise en page du texte Parcours

--- src/components/Parcours/Parcours.js
+++ src/components/Parcours/Parcours.js
@@ -18,7 +18,8 @@ export default function Parcours() {
                     C’est le moment où mon aîné commence à aller à l’école mais,
                     avec mon épouse, nous nous rendons compte pendant sa
                     deuxième année de maternelle que ce système ne lui convient
-                    pas. Nous décidons de déménager de Colomiers vers Albi (je
+                    pas.
+                    <br /> Nous décidons de déménager de Colomiers vers Albi (je
                     garde mon emploi sur la région toulousaine, travaillant de
                     week-end à cette période de ma vie) pour le placer en école
                     Montessori . Je commence à m’intéresser à cette pédagogie
@@ -30,19 +31,21 @@ export default function Parcours() {
                     temps ont raison de ma volonté de continuer dans mon secteur
                     d’activité et je crée mon auto-entreprise qui propose du
                     soutien scolaire niveau collège et des ateliers Montessori à
-                    destination d’enfants trisomiques. La directrice de l’école
-                    dans laquelle sont scolarisés mes enfants me prête les
-                    locaux de l’école (je ne la remercierai jamais assez) pour
-                    mon activité qui se déroule le mercredi. Le reste de la
-                    semaine, je l’occupe en donnant un coup de main à l’école
-                    (surveillance pendant les temps de repas et de garderie).
-                    Lorsqu’une collègue tombe malade, la directrice me propose
-                    de m’embaucher au sein de l’école et c’est le départ d’une
-                    nouvelle aventure qui durera 4 années.
+                    destination d’enfants trisomiques.
+                    <br /> La directrice de l’école dans laquelle sont
+                    scolarisés mes enfants me prête les locaux de l’école (je ne
+                    la remercierai jamais assez) pour mon activité qui se
+                    déroule le mercredi. Le reste de la semaine, je l’occupe en
+                    donnant un coup de main à l’école (surveillance pendant les
+                    temps de repas et de garderie).
+                    <br /> Lorsqu’une collègue tombe malade, la directrice me
+                    propose de m’embaucher au sein de l’école et c’est le départ
+                    d’une nouvelle aventure qui durera 4 années.
                 </p>
                 <p>
                     Malheureusement, le confinement pendant la pandémie et des
                     problèmes de santé me forceront à arrêter cette activité.
+                    <br />
                     Nous faisons le choix avec mon épouse de nous « mettre au
                     vert » et de ralentir le rythme effréné dans lequel nous
                     nous étions enfermés et nous déménageons une fois de plus,
@@ -59,7 +62,8 @@ export default function Parcours() {
                     faisant quelques recherches avant de choisir la plateforme
                     de formation, que la gestion de contenu sur les réseaux
                     sociaux ne m’est pas aussi aisée que je l’avais pensé au
-                    départ. L’évidence s’impose clairement à moi : puisque j’ai
+                    départ.
+                    <br /> L’évidence s’impose clairement à moi : puisque j’ai
                     aidé mon épouse et une amie à mettre leur site en place sous
                     Wordpress, je n’ai qu’à continuer dans cette voie. Mon choix
                     est alors fait : je me tourne vers le développement Web.
@@ -67,10 +71,11 @@ export default function Parcours() {
                 <p>
                     En quelques jours, le dossier est monté auprès de Pôle
                     Emploi et je débute officiellement ma formation chez
-                    OpenClassrooms le 1er juillet 2022. Finalement, après
-                    quelques frayeurs pour avoir pris un peu de retard sur un
-                    projet, je vais pouvoir terminer ma formation avec une
-                    semaine d’avance sur le programme prévu, le 21 février.
+                    OpenClassrooms le 1er juillet 2022.
+                    <br /> Finalement, après quelques frayeurs pour avoir pris
+                    un peu de retard sur un projet, je vais pouvoir terminer ma
+                    formation avec une semaine d’avance sur le programme prévu,
+                    le 21 février.
                 </p>
                 <p>
                     Il est temps maintenant de rejoindre le grand bain de la
