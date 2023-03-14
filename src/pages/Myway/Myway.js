c7e7beb4e2e53fbf3c483be245dbe96f6907a9c3
David Isidore <isidoredavid@protonmail.com>
2023-03-14 11:02:26 +0100

    Implémentation de la partie Parcours

--- src/pages/Myway/Myway.js
+++ src/pages/Myway/Myway.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
+import Parcours from '../../components/Parcours/Parcours'
 
 export default function Myway() {
     return (
         <div>
             <Header />
+            <Parcours />
             <Footer />
         </div>
     )
